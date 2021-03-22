39133f79a7da0c3f7502a27f5649f17ac0663b16
phamthekiem <61005592+phamthekiem@users.noreply.github.com>
2021-03-22 14:07:29 +0700

    Update khocode-wp.php

--- khocode-wp.php
+++ khocode-wp.php
@@ -886,6 +886,17 @@ Thay đổi ký tự tiền tệ Đồng Việt Nam
 
 <!--  -->
 
+Thay đổi vị trí của giá bán và giá khuyến mãi trong WooCommerce của wordpress
+
+<?php 
+add_filter( 'woocommerce_format_sale_price', 'invert_formatted_sale_price', 10, 3 );
+function invert_formatted_sale_price( $price, $regular_price, $sale_price ) {
+    return '<ins>' . ( is_numeric( $sale_price ) ? wc_price( $sale_price ) : $sale_price ) . '</ins> <del>' . ( is_numeric( $regular_price ) ? wc_price( $regular_price ) : $regular_price ) . '</del>';
+}
+?>
+
+<!--  -->
+
 11. Code lấy nội dung mô tả ngắn, category, từ khóa của sản phẩm.
 
 <?php echo apply_filters( 'woocommerce_short_description', $post->post_excerpt ) ?>
